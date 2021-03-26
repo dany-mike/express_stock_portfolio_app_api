c488ca86889b9faae1f8d63e49c33aa16d922517
Dany <danymike.joseph@gmail.com>
2021-03-26 18:55:16 +0100

    feat: get forecast data with tipranks-api-v2

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const cors = require('cors');
 const app = express();
 
 const auth = require('./src/middlewares/auth.middleware');
-const {getUsers, userLogin} = require('./src/controllers/users.controller');
 const serialization = require("./src/middlewares/serialization.middleware");
+
+
+const {getUsers, userLogin} = require('./src/controllers/users.controller');
 const getHistoricalDataStock = require('./src/controllers/getHistoricalDataStock.controller');
+const getForecastData = require('./src/controllers/getForecastData.controller');
 
 
 app.use(express.json());
@@ -17,8 +20,10 @@ app.get('/users', getUsers ,serialization);
 
 app.post('/users', userLogin, serialization)
 
+app.get('/forecast/:symbol', getForecastData.getPriceTargets , serialization);
+
 app.get('/historical/:symbol/:from/:to', getHistoricalDataStock , serialization);
 
-app.listen(3000, 'localhost', () => {
+app.listen(process.env.PORT, 'localhost', () => {
     console.log('started');
 })
