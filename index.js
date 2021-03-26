c1b0c84b4feeb79f4bb4cedbe3c76430570c0702
Dany <danymike.joseph@gmail.com>
2021-03-26 10:48:47 +0100

    install cors into my project

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
 const auth = require('./src/middlewares/auth.middleware');
 const {getUsers, userLogin} = require('./src/controllers/users.controller');
 const serialization = require("./src/middlewares/serialization.middleware");
 const getHistoricalDataStock = require('./src/controllers/getHistoricalDataStock.controller');
 
-app.use(express.json())
 
-// List of users URI
+app.use(express.json());
+
 app.get('/users', getUsers ,serialization);
 
 app.post('/users', userLogin, serialization)
 
-// Historical Data URI
 app.get('/historical/:symbol/:from/:to', getHistoricalDataStock , serialization);
 
 app.listen(3000, 'localhost', () => {
