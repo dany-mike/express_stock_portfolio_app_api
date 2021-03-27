be7593e650c101317a6edc43d377754d663989fc
Dany <danymike.joseph@gmail.com>
2021-03-27 12:06:42 +0100

    feat: price-target news-sentiment trending-stock endpoint

--- index.js
+++ index.js
@@ -11,16 +11,22 @@ const serialization = require("./src/middlewares/serialization.middleware");
 
 const {getUsers, userLogin} = require('./src/controllers/users.controller');
 const getHistoricalDataStock = require('./src/controllers/getHistoricalDataStock.controller');
-const getForecastData = require('./src/controllers/getForecastData.controller');
+const forecast = require('./src/services/forecast.service')
 
-
+// Data Parsing
 app.use(express.json());
+
+// Allow access-control-allow-origin
+app.use(cors());
 
 app.get('/users', getUsers ,serialization);
 
-app.post('/users', userLogin, serialization)
+app.post('/users', userLogin, serialization);
 
-app.get('/forecast/:symbol', getForecastData.getPriceTargets , serialization);
+app.get('/forecast/price-target/:symbol', forecast.getPriceTargets , serialization);
+app.get('/forecast/news-sentiment/:symbol', forecast.getNewsSentimentData , serialization);
+app.get('/forecast/trending-stock/', forecast.getTrendingStocks , serialization);
+
 
 app.get('/historical/:symbol/:from/:to', getHistoricalDataStock , serialization);
 
