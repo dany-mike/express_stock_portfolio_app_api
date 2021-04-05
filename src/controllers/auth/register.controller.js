ee697286b79c28f84d653b6146f95df4d702e3d0
Dany <danymike.joseph@gmail.com>
2021-04-05 10:15:28 +0200

    create validation.util.js and test validation on register.controller.js

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -1,44 +1,31 @@
 const User = require('../../models/User.model');
-
-// Validation 
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().email().min(6).required(),
-    password: Joi.string().min(6).required()
-})
+const validation  = require('../../utils/validation.util');
 
 async function register(req, res, next) {
 
-    // Validate Data
-    const validation = schema.validate(req.body);
+    const validate = validation.registerValidation(req.body)
 
-    if(validation.error) {
-        res.send(validation.error.details[0])
-    } else {
-        res.send("Validation ok !")
+    if(validate.error) {
+        res.status(400)
+        .send(validate.error.details[0].message)
     }
 
     // Create a new User instance
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    // });
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
 
-    // try {
-    //     const savedUser = await user.save()
-    //     res.rawResponse = savedUser
-    //     return next()
+    try {
+        const savedUser = await user.save()
+        res.rawResponse = savedUser
+        return next()
 
-    // } catch(err) {
-    //     res.rawResponse = res.status(400)
-    //     return next()
-    // }
-    // Save into the database
-   
-
+    } catch(err) {
+        res.rawResponse = res.status(400)
+        return next()
+    }   
 }
 
 module.exports = register
