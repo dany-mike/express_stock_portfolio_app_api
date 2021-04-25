ecb960bf1a705c76ca148b6367782cb244c0ad95
Dany <danymike.joseph@gmail.com>
2021-04-25 11:47:20 +0200

    feat each user can create a new wallet endpoint

---
+++ src/controllers/wallet/createWallet.controller.js
@@ -0,0 +1,32 @@
+const Wallet = require('../../models/Wallet.model')
+const User = require('../../models/User.model')
+
+async function createWallet(req, res, next) {
+    const user = await User.findOne({username: req.params.username})
+    console.log(user)
+
+    if(!user) {
+        res.rawStatus = 400
+        res.rawResponse = `Username: ${req.params.username} does not exists`
+        return next();
+    }
+
+    const wallet = new Wallet({
+        walletName: req.body.walletName,
+        description: req.body.description,
+        user: user._id,
+    })
+
+    try {
+        wallet.save()
+        res.rawStatus = 200
+        res.rawResponse = "Wallet created successfully !"
+        return next()
+    } catch(err) {
+        res.rawStatus = 500
+        res.rawResponse = err.message
+        return next()
+    }
+}
+
+module.exports = createWallet
--- src/routes/wallet.route.js
+++ src/routes/wallet.route.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 const addStockToWallet = require('../controllers/wallet/addStockToWallet.controller')
+const createWallet = require('../controllers/wallet/createWallet.controller')
 const deleteStock = require('../controllers/wallet/deleteStock.controller')
 const editStock = require('../controllers/wallet/editStock.controller')
 const getWalletByUserId = require('../controllers/wallet/getWalletByUserId.controller')
 const serialization = require('../middlewares/serialization.middleware')
 const verify = require('./verifyToken.route')
+
+router.post('/add-wallet/:username', verify, createWallet, serialization)
 
 router.get('/:user_id/',verify, getWalletByUserId, serialization)
 
