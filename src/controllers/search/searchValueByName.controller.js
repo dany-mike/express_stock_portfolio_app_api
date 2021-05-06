1694d8d408e6f27a670c5c984c2dec81f0a58527
Dany <danymike.joseph@gmail.com>
2021-05-06 17:18:13 +0200

    issue fixed when empty array in searchValueByName.controller.js

--- src/controllers/search/searchValueByName.controller.js
+++ src/controllers/search/searchValueByName.controller.js
@@ -2,15 +2,21 @@ const Value = require("../../models/Value.model");
 const toUpFLetter = require ('../../utils/toUppercaseFirstLetter.util')
 
 async function searchValuesByName(req, res, next) {
-  const value = req.body.Name;
+  const value = req.query.name;
+
+  if(value === "") {
+    res.rawStatus = 200;
+    res.rawResponse = [];
+    return next();
+  }
+  
   const uValue = toUpFLetter(value)
 
-  const regex = new RegExp("^" + uValue);
-  
   try {
+    const regex = new RegExp("^" + uValue);
     const values = await Value.find({
       Name: regex,
-    });
+    }).limit(7)
     res.rawStatus = 200;
     res.rawResponse = values;
     return next();
