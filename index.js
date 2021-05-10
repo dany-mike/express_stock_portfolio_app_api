a574793a0a732d5ebff96d620767b0fda35aef7d
Dany <danymike.joseph@gmail.com>
2021-05-10 16:13:24 +0200

    remove / on cors policy localhost

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: `http://localhost:${process.env.PORT_FRONT}/`,
+    origin: `http://localhost:${process.env.PORT_FRONT}`,
   })
 );
 
