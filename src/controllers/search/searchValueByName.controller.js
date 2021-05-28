ccdb1c8935a276fe03e03ac62e693a62894b13a9
Dany <danymike.joseph@gmail.com>
2021-05-28 13:49:39 +0200

    feat: searchValue endpoint edited (by sector and name)

--- src/controllers/search/searchValueByName.controller.js
+++ src/controllers/search/searchValueByName.controller.js
@@ -1,25 +1,48 @@
 const Value = require("../../models/Value.model");
-const toUpFLetter = require ('../../utils/toUppercaseFirstLetter.util')
+const toUpFLetter = require("../../utils/toUppercaseFirstLetter.util");
 
 async function searchValuesByName(req, res, next) {
   const value = req.query.name;
+  const sector = req.query.sector;
 
-  if(value === "") {
+  if (value === "") {
     res.rawStatus = 200;
     res.rawResponse = [];
     return next();
   }
-  
-  const uValue = toUpFLetter(value)
 
   try {
-    const regex = new RegExp("^" + uValue);
-    const values = await Value.find({
-      Name: regex,
-    }).limit(7)
-    res.rawStatus = 200;
-    res.rawResponse = values;
-    return next();
+    if (req.query.sector && req.query.name) {
+      const uValue = toUpFLetter(value);
+      const regex = new RegExp("^" + uValue);
+
+      const values = await Value.find({
+        Name: regex,
+        Sector: sector,
+      }).limit(7);
+      res.rawStatus = 200;
+      res.rawResponse = values;
+      return next();
+    }
+    if (req.query.name) {
+      const uValue = toUpFLetter(value);
+      const regex = new RegExp("^" + uValue);
+
+      const values = await Value.find({
+        Name: regex,
+      }).limit(7);
+      res.rawStatus = 200;
+      res.rawResponse = values;
+      return next();
+    }
+    if (req.query.sector) {
+      const values = await Value.find({
+        Sector: sector,
+      }).limit(7);
+      res.rawStatus = 200;
+      res.rawResponse = values;
+      return next();
+    }
   } catch (err) {
     res.rawStatus = 500;
     res.rawResponse = err.message;
